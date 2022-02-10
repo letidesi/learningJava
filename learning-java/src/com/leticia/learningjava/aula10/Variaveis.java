c4795a06316911b4e8e244ee69d1e24aea023664
Leticia Desiderio <letidesiderio18@gmail.com>
2022-02-10 15:09:09 -0300

    aprendendo java com a IDE Eclipse

---
+++ learning-java/src/com/leticia/learningjava/aula10/Variaveis.java
@@ -0,0 +1,19 @@
+package com.leticia.learningjava.aula10;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+		int idade = 20;
+		String nome = "Leticia";
+		String nomeDoMeuCachorro = "Simba";
+		int ano = 2000;
+		
+		idade = 21;
+		nome = "Letícia";
+		
+		System.out.println("A " + nome + " tem " + idade + " anos, o nome do cachorro dela é " + nomeDoMeuCachorro + ". Letícia nasceu em " + ano + ".");
+		
+	}
+
+}
