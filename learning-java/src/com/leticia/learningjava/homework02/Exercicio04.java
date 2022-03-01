8d5698e0d47a4fb43e36b47a94aa02bed9b02b8b
Leticia Desiderio <letidesiderio18@gmail.com>
2022-02-28 22:34:32 -0300

    exercício informa se a letra é uma vogal ou consoante

--- learning-java/src/com/leticia/learningjava/homework02/Exercicio04.java
+++ learning-java/src/com/leticia/learningjava/homework02/Exercicio04.java
@@ -17,17 +17,17 @@ public class Exercicio04 {
 		String letra = scanner.next();
 		
 		switch(letra) {
-		case "a": System.out.println("A letra " + letra + " é uma vogal."); break;
-		case "A": System.out.println("A letra " + letra + " é uma vogal."); break;
-		case "e": System.out.println("A letra " + letra + " é uma vogal."); break;
-		case "E": System.out.println("A letra " + letra + " é uma vogal."); break;
-		case "i": System.out.println("A letra " + letra + " é uma vogal."); break;
-		case "I": System.out.println("A letra " + letra + " é uma vogal."); break;
-		case "o": System.out.println("A letra " + letra + " é uma vogal."); break;
-		case "O": System.out.println("A letra " + letra + " é uma vogal."); break;
-		case "u": System.out.println("A letra " + letra + " é uma vogal."); break;
-		case "U": System.out.println("A letra " + letra + " é uma vogal."); break;
-		default: System.out.println("A letra " + letra + " é uma consoante."); break;
+		case "a": 
+		case "A": 
+		case "e": 
+		case "E": 
+		case "i": 
+		case "I": 
+		case "o": 
+		case "O": 
+		case "u": 
+		case "U": System.out.println("A letra '" + letra + "' é uma vogal."); break;
+		default: System.out.println("A letra '" + letra + "' é uma consoante."); break;
 		}
 
 	}
