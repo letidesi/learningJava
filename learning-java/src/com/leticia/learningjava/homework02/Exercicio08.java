11ecd683ba21b0bee1d66f8741c3dc3215909d8f
Leticia Desiderio <letidesiderio18@gmail.com>
2022-03-01 00:36:35 -0300

    exercício informa qual é o produto mais barato para se comprar

--- learning-java/src/com/leticia/learningjava/homework02/Exercicio08.java
+++ learning-java/src/com/leticia/learningjava/homework02/Exercicio08.java
@@ -7,6 +7,7 @@ public class Exercicio08 {
 	public static void main(String[] args) {
 
 		/*
+		 * 
 		 * Faça um programa que pergunte o preço de três produtos e informe qual produto
 		 * você deve comprar, sabendo que a decisão é sempre pelo mais barato.
 		 * 
