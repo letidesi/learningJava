8e9150cd882037e55761503fac6cbb474117fba1
Leticia Desiderio <letidesiderio18@gmail.com>
2022-03-01 00:30:47 -0300

    exercício informa se a letra é uma vogal ou consoante.

--- learning-java/src/com/leticia/learningjava/homework02/Exercicio04.java
+++ learning-java/src/com/leticia/learningjava/homework02/Exercicio04.java
@@ -5,36 +5,38 @@ import java.util.Scanner;
 public class Exercicio04 {
 
 	public static void main(String[] args) {
-		
+
 		/*
-		   Faça um programa que verifique se uma letra digitada é vogal ou consoante.
-		   
+		 * Faça um programa que verifique se uma letra digitada é vogal ou consoante.
+		 * 
 		 */
-		
+
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.println("Informe uma letra: ");
 		String letra = scanner.next();
-		
-		if(letra.length() > 1) {
+
+		if (letra.length() > 1) {
 			System.out.println("Não é uma letra válida.");
 		} else {
-				switch(letra) {
-				case "a": 
-				case "A": 
-				case "e": 
-				case "E": 
-				case "i": 
-				case "I": 
-				case "o": 
-				case "O": 
-				case "u": 
-				case "U": System.out.println("A letra '" + letra + "' é uma vogal."); break;
-				default: System.out.println("A letra '" + letra + "' é uma consoante."); break;
-				}
+			switch (letra) {
+			case "a":
+			case "A":
+			case "e":
+			case "E":
+			case "i":
+			case "I":
+			case "o":
+			case "O":
+			case "u":
+			case "U":
+				System.out.println("A letra '" + letra + "' é uma vogal.");
+				break;
+			default:
+				System.out.println("A letra '" + letra + "' é uma consoante.");
+				break;
+			}
 		}
-		
-	
 
 	}
 
