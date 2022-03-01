a35eebc6e0784d56c55c608aac11fed174c04f37
Leticia Desiderio <letidesiderio18@gmail.com>
2022-03-01 00:33:00 -0300

    exercício informa o maior número

--- learning-java/src/com/leticia/learningjava/homework02/Exercicio06.java
+++ learning-java/src/com/leticia/learningjava/homework02/Exercicio06.java
@@ -5,26 +5,26 @@ import java.util.Scanner;
 public class Exercicio06 {
 
 	public static void main(String[] args) {
-		
+
 		/*
-		    Faça um programa que leia três números e mostre o maior deles.
-		    
+		 * Faça um programa que leia três números e mostre o maior deles.
+		 * 
 		 */
-		
+
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.println("Informe o primeiro número: ");
-		double primeiroNumero = scanner.nextDouble(); 
-		
+		double primeiroNumero = scanner.nextDouble();
+
 		System.out.println("Informe o segundo número: ");
-		double segundoNumero = scanner.nextDouble(); 
-		
+		double segundoNumero = scanner.nextDouble();
+
 		System.out.println("Informe o terceiro número");
-		double terceiroNumero = scanner.nextDouble(); 
-		
-		if(primeiroNumero >= segundoNumero && primeiroNumero >= terceiroNumero) {
+		double terceiroNumero = scanner.nextDouble();
+
+		if (primeiroNumero >= segundoNumero && primeiroNumero >= terceiroNumero) {
 			System.out.println("O primeiro número é o maior: " + primeiroNumero);
-		} else if(segundoNumero >= primeiroNumero && segundoNumero >= terceiroNumero) {
+		} else if (segundoNumero >= primeiroNumero && segundoNumero >= terceiroNumero) {
 			System.out.println("O segundo número é o maior: " + segundoNumero);
 		} else if (terceiroNumero >= primeiroNumero && terceiroNumero >= segundoNumero) {
 			System.out.println("O terceiro número é o maior: " + terceiroNumero);
