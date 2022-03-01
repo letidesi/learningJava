74682363719a3dac8a756ff8fe8854aacd5c50f5
Leticia Desiderio <letidesiderio18@gmail.com>
2022-03-01 00:35:22 -0300

    exercício informa qualé o produto mais barato para se comprar

--- learning-java/src/com/leticia/learningjava/homework02/Exercicio08.java
+++ learning-java/src/com/leticia/learningjava/homework02/Exercicio08.java
@@ -5,32 +5,32 @@ import java.util.Scanner;
 public class Exercicio08 {
 
 	public static void main(String[] args) {
-		
+
 		/*
-		   Faça um programa que pergunte o preço de três produtos e informe qual
-		   produto você deve comprar, sabendo que a decisão é sempre pelo mais barato.
-		   
+		 * Faça um programa que pergunte o preço de três produtos e informe qual produto
+		 * você deve comprar, sabendo que a decisão é sempre pelo mais barato.
+		 * 
 		 */
-		
-		Scanner scanner = new Scanner(System.in); 
-		
-		System.out.println("Informe o preço do primeiro produto: "); 
-		double primeiroPreco= scanner.nextDouble();
-		
-		System.out.println("Informe o preço do segundo produto: "); 
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Informe o preço do primeiro produto: ");
+		double primeiroPreco = scanner.nextDouble();
+
+		System.out.println("Informe o preço do segundo produto: ");
 		double segundoPreco = scanner.nextDouble();
-		
-		System.out.println("Informe o preço do terceiro produto: "); 
+
+		System.out.println("Informe o preço do terceiro produto: ");
 		double terceiroPreco = scanner.nextDouble();
-			
-		if(primeiroPreco <= segundoPreco && primeiroPreco <= terceiroPreco) {
+
+		if (primeiroPreco <= segundoPreco && primeiroPreco <= terceiroPreco) {
 			System.out.println("Compre o primeiro produto com o valor de: " + primeiroPreco);
 		} else if (segundoPreco <= primeiroPreco && segundoPreco <= terceiroPreco) {
 			System.out.println("Compre o segundo produto com o valor de: " + segundoPreco);
 		} else if (terceiroPreco <= primeiroPreco && terceiroPreco <= segundoPreco) {
 			System.out.println("Compre o terceiro produto com o valor de: " + terceiroPreco);
 		}
-		
+
 	}
 
 }
