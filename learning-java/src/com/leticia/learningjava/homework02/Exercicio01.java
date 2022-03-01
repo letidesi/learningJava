1f44f70c93a24290f84136e0938d5a153de203c4
Leticia Desiderio <letidesiderio18@gmail.com>
2022-03-01 00:25:46 -0300

    exercício de imprimir o maior número

--- learning-java/src/com/leticia/learningjava/homework02/Exercicio01.java
+++ learning-java/src/com/leticia/learningjava/homework02/Exercicio01.java
@@ -5,21 +5,21 @@ import java.util.Scanner;
 public class Exercicio01 {
 
 	public static void main(String[] args) {
-		
-		/* 
-		   Faça um programa que peça dois números e imprima o maior deles.
-		   
+
+		/*
+		 * Faça um programa que peça dois números e imprima o maior deles.
+		 * 
 		 */
-		
+
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.println("Informe o primeiro número: ");
 		double primeiroNumero = scanner.nextDouble();
-		
+
 		System.out.println("Informe o segundo número: ");
 		double segundoNumero = scanner.nextDouble();
-		
-		if(primeiroNumero > segundoNumero) {
+
+		if (primeiroNumero > segundoNumero) {
 			System.out.println("O maior número é: " + primeiroNumero);
 		} else {
 			System.out.println("O maior número é: " + segundoNumero);
