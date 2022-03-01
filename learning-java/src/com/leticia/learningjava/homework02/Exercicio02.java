ed1974af731034ee27594ccf2e9b2e64b5bc1747
Leticia Desiderio <letidesiderio18@gmail.com>
2022-03-01 00:27:35 -0300

    exercício informa se o número é positivo ou negativo.

--- learning-java/src/com/leticia/learningjava/homework02/Exercicio02.java
+++ learning-java/src/com/leticia/learningjava/homework02/Exercicio02.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 public class Exercicio02 {
 
 	public static void main(String[] args) {
-		
+
 		/*
-		   Faça um programa que peça um valor e mostre na tela 
-		   se o valor é positivo ou negativo.
-		   
+		 * Faça um programa que peça um valor e mostre na tela se o valor é positivo ou
+		 * negativo.
+		 * 
 		 */
-		
+
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.println("Informe um número: ");
 		double numero = scanner.nextDouble();
-		
-		if(numero < 0) {
+
+		if (numero < 0) {
 			System.out.println("O número informado foi: '" + numero + "'. É negativo!");
 		} else {
 			System.out.println("O número informado foi: '" + numero + "'. É positivo!");
