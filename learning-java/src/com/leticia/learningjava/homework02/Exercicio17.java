a0824ba17144ec2a5e3996d1369abe72cb1ff0c5
Leticia Desiderio <letidesiderio18@gmail.com>
2022-03-03 10:27:42 -0300

    exercício mostra se o ano é bissexto ou não.

--- learning-java/src/com/leticia/learningjava/homework02/Exercicio17.java
+++ learning-java/src/com/leticia/learningjava/homework02/Exercicio17.java
@@ -5,24 +5,23 @@ import java.util.Scanner;
 public class Exercicio17 {
 
 	public static void main(String[] args) {
-		
-		/*
-		   Faça um programa que peça um número correspondente a um determinado
-		   ano e em seguida informe se este ano é ou não bissexto.
-		   
-		 */
-		
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.println("Informe um ano: ");
-	    int ano = scanner.nextInt();
 
-	    
-	    if(ano % 400 == 0 || ano % 4 == 0 && ano % 100 != 0 ) {
-	    	System.out.println("O ano de " + ano + " é bissexto.");
-	    } else {
-	    	System.out.println("O ano de " + ano + " não é bissexto.");
-	    }
-	    
-	 }
+		/*
+		 * Faça um programa que peça um número correspondente a um determinado ano e em
+		 * seguida informe se este ano é ou não bissexto.
+		 * 
+		 */
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Informe um ano: ");
+		int ano = scanner.nextInt();
+
+		if (ano % 400 == 0 || ano % 4 == 0 && ano % 100 != 0) {
+			System.out.println("O ano de " + ano + " é bissexto.");
+		} else {
+			System.out.println("O ano de " + ano + " não é bissexto.");
+		}
+
+	}
 }
