c115e964ab6c648ff64702ba1847f1e9f2806ff7
Leticia Desiderio <letidesiderio18@gmail.com>
2022-03-03 09:58:27 -0300

    exercício mostra se o número é par ou ímpar.

---
+++ learning-java/src/com/leticia/learningjava/homework02/Exercicio18.java
@@ -0,0 +1,28 @@
+package com.leticia.learningjava.homework02;
+
+import java.util.Scanner;
+
+public class Exercicio18 {
+
+	public static void main(String[] args) {
+		
+		/*
+		   Faça um programa que peça um número inteiro e determine
+		   se ele é par ou ímpar.
+		   
+		 */
+
+      Scanner scanner = new Scanner(System.in);
+      
+      System.out.println("Informe um número inteiro: ");
+      int numero = scanner.nextInt();
+      
+      if(numero % 2 == 0) {
+    	  System.out.println("É par.");
+      } else {
+    	  System.out.println("É ímpar.");
+      }
+      
+	}
+
+}
