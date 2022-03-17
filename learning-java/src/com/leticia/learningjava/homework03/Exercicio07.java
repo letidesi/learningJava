61d47d2f0ec6028a416f9e3be5e6c2968195609c
Leticia Desiderio <letidesiderio18@gmail.com>
2022-03-17 11:21:01 -0300

    o exercício imprime na tela o maior valor de 5 números informado pelo usuário.

--- learning-java/src/com/leticia/learningjava/homework03/Exercicio07.java
+++ learning-java/src/com/leticia/learningjava/homework03/Exercicio07.java
@@ -5,29 +5,27 @@ import java.util.Scanner;
 public class Exercicio07 {
 
 	public static void main(String[] args) {
-		
+
 		/*
 		 * Faça um programa que leia 5 números e informe o maior número.
 		 */
-		
+
 		Scanner scanner = new Scanner(System.in);
-		
+
 		double maior = 0;
-		
-		for(int i = 0; i < 5; i++) {
-			
+
+		for (int i = 0; i < 5; i++) {
+
 			System.out.println("Infome um número:");
 			double numeroInformado = scanner.nextDouble();
-			
-			if(numeroInformado > maior) {
-				
+
+			if (numeroInformado > maior) {
+
 				maior = numeroInformado;
 			}
-
 		}
-		
+
 		System.out.println("O maior número é: " + maior);
-		
 	}
- 
+
 }
