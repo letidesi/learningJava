3681a804c6cc2793c7a5621d7a24c51de4e8e2f5
Leticia Desiderio <letidesiderio18@gmail.com>
2022-03-17 11:34:38 -0300

    o exercício imprime na tela a soma e a média dos números.

--- learning-java/src/com/leticia/learningjava/homework03/Exercicio08.java
+++ learning-java/src/com/leticia/learningjava/homework03/Exercicio08.java
@@ -5,25 +5,25 @@ import java.util.Scanner;
 public class Exercicio08 {
 
 	public static void main(String[] args) {
-		
+
 		/*
 		 * Faça um programa que leia 5 números e informe a soma e a média dos números.
 		 */
-		
+
 		Scanner scanner = new Scanner(System.in);
 
-		double soma = 0 ;
+		double soma = 0;
 		double media = 0;
 
 		for (int i = 0; i < 5; i++) {
 
 			System.out.println("Informe um número:");
 			double num = scanner.nextDouble();
-           
+
 			soma += num;
-			
+
 			media = soma / 5;
-			
+
 		}
 
 		System.out.println("O valor da soma dos números é: " + soma + "\n" + "A média é: " + media);
